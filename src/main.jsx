7111644a1e0215b41069d9298f538c51150813a2
Shanto93 <shantoislam7363@gmail.com>
2024-08-23 12:53:05 +0600

    Dynamic title for this project is done

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import {
-  RouterProvider,
-} from "react-router-dom";
-import { router } from './Routes/Routes';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/Routes";
+import { HelmetProvider } from "react-helmet-async";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <HelmetProvider>
+      <div className="max-w-screen-lg mx-auto">
+        <RouterProvider router={router} />
+      </div>
+    </HelmetProvider>
+  </StrictMode>
+);
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,14 @@
+import { Helmet } from "react-helmet-async";
+
 const Home = () => {
-    return (
-        <div>
-            <h2 className="text-4xl">This is Homepage</h2>
-        </div>
-    );
+  return (
+    <div>
+      <Helmet>
+        <title>Story Tale | Home</title>
+      </Helmet>
+      <h2 className="text-4xl">This is Homepage</h2>
+    </div>
+  );
 };
 
 export default Home;
